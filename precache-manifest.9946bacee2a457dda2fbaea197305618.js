61467a6060cef05bbe8b5bf5235b108525ad15cd
puneet-ahuja <ahujapuneet92@gmail.com>
2019-10-27 18:11:47 +0530

    Updates

---
+++ precache-manifest.9946bacee2a457dda2fbaea197305618.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa0929cf6cd3cbcc062e13dbd44d551",
+    "url": "/trello/index.html"
+  },
+  {
+    "revision": "21f9e0cd215746e3bdc5",
+    "url": "/trello/static/css/main.d4c33851.chunk.css"
+  },
+  {
+    "revision": "4aa5bc6c070dcebac949",
+    "url": "/trello/static/js/2.93582d55.chunk.js"
+  },
+  {
+    "revision": "21f9e0cd215746e3bdc5",
+    "url": "/trello/static/js/main.9085494c.chunk.js"
+  },
+  {
+    "revision": "09b57b538c6b90579e09",
+    "url": "/trello/static/js/runtime-main.b77b009c.js"
+  }
+]);
